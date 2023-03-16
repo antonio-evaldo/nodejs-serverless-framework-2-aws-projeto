42adee37d41e11bc025dcc85a96e39665af33005
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-03-16 13:17:56 -0300

    usando arquivo CSV com bucket local

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { readFile } = require("fs/promises");
 const { parse } = require("fast-csv");
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 module.exports.simulandoUploadDeBucket = async () => {
   try {
-    const client = new S3Client({
+    const cliente = new S3Client({
       forcePathStyle: true,
       credentials: {
         accessKeyId: "S3RVER",
@@ -12,10 +13,12 @@ module.exports.simulandoUploadDeBucket = async () => {
       endpoint: "http://localhost:4569"
     });
 
-    await client.send(new PutObjectCommand({
+    const dadosCsv = await readFile(`../cadastro_usuarios.csv`, "utf-8");
+
+    await cliente.send(new PutObjectCommand({
       Bucket: "bucket-local",
       Key: "1234",
-      Body: Buffer.from("abcd")
+      Body: Buffer.from(dadosCsv)
     }));
 
     return {
@@ -32,48 +35,54 @@ module.exports.simulandoUploadDeBucket = async () => {
   }
 };
 
-module.exports.cadastrarAlunos = async (evento, contexto) => {
-  console.log("\n\nevento:");
-  console.log(JSON.stringify(evento));
+module.exports.cadastrarAlunos = async (evento) => {
+  try {
+    const nomeBucket = evento.Records[0].s3.bucket.name;
+    const chaveBucket = decodeURIComponent(evento.Records[0].s3.object.key.replace(/\+/g, ' '));
 
-    // const s3 = new aws.S3();
+    const dadosCsv = await readFile(`./buckets/${nomeBucket}/${chaveBucket}._S3rver_object`, "utf-8");
 
-    // const Bucket = evento.Records[0].s3.bucket.name;
-    // const Key = decodeURIComponent(evento.Records[0].s3.object.key.replace(/\+/g, ' '));
-    // const objetoBucket = await s3.getObject({ Bucket, Key }).promise();
+    // const cliente = new S3Client();
+
+    // const objetoBucket = await cliente.send(
+    //   new GetObjectCommand({ Bucket, Key })
+    // );
 
     // const dadosCsv = objetoBucket.Body.toString("utf-8");
 
-    // const resultado = await new Promise((resolver, rejeitar) => {
-    //   const alunosPromessas = [];
+    const resultado = await new Promise((resolver, rejeitar) => {
+      const alunosPromessas = [];
 
-    //   const stream = parse({ headers: ["nome", "matricula"], renameHeaders: true })
-    //     .on("error", (erro) => rejeitar(erro))
-    //     .on("data", (aluno) => {
-    //       const promessa = fetch("http://ecs-django-186565849.us-east-1.elb.amazonaws.com/alunos", {
-    //         method: "POST",
-    //         body: JSON.stringify(aluno),
-    //         headers: { 'Content-Type': 'application/json' }
-    //       })
+      const stream = parse({ headers: ["nome", "matricula"], renameHeaders: true })
+        .on("error", (erro) => rejeitar(erro))
+        .on("data", (aluno) => {
+          const promessa = fetch("http://ecs-django-186565849.us-east-1.elb.amazonaws.com/alunos", {
+            method: "POST",
+            body: JSON.stringify(aluno),
+            headers: { 'Content-Type': 'application/json' }
+          })
 
-    //       alunosPromessas.push(promessa);
-    //     })
-    //     .on("end", () => {
-    //       resolver(alunosPromessas);
-    //     });
+          alunosPromessas.push(promessa);
+        })
+        .on("end", () => {
+          resolver(alunosPromessas);
+        });
 
-    //   stream.write(dadosCsv);
-    //   stream.end();
-    // });
+      stream.write(dadosCsv);
+      stream.end();
+    });
 
-    // if (Array.isArray(resultado)) {
-    //   const alunos = await Promise.all(resultado);
+    if (Array.isArray(resultado)) {
+      const alunos = await Promise.all(resultado);
 
-    //   console.log({
-    //     message: `${resultado.length} alunos foram cadastrados.`,
-    //     alunos
-    //   });
-    // } else {
-    //   console.log(resultado);
-    // }
+      console.log({
+        message: `${resultado.length} alunos foram cadastrados.`,
+        alunos
+      });
+    } else {
+      console.log(resultado);
+    }
+  } catch (erro) {
+    console.log(erro);
+  }
 }
